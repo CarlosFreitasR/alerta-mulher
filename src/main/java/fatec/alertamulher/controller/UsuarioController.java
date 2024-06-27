817f95ce88cba166b9bdb9c50c0fc7511d30419a
CarlosFreitasR <carlos.ribeiro@mt.estudante.senai.br>
2024-06-26 20:16:27 -0400

    configurando thymeleaf

--- src/main/java/fatec/alertamulher/controller/UsuarioController.java
+++ src/main/java/fatec/alertamulher/controller/UsuarioController.java
@@ -4,17 +4,16 @@ import fatec.alertamulher.model.UsuarioDto;
 import fatec.alertamulher.model.UsuarioForm;
 import fatec.alertamulher.service.UsuarioService;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/usuario")
-@RestController
+@Controller
 @AllArgsConstructor(onConstructor_ = @__(@Autowired))
 public class UsuarioController {
     private final UsuarioService service;
@@ -23,5 +22,12 @@ public class UsuarioController {
     public ResponseEntity<UsuarioDto> criarUsuario(@RequestBody @Validated UsuarioForm usuarioForm) {
         UsuarioDto usuarioDto = service.criarUsuario(usuarioForm);
         return ResponseEntity.ok().body(usuarioDto);
+    }
+
+    @GetMapping("/home/{pk}")
+    public String mostrarUsuarios(@PathVariable Long pk) {
+        UsuarioDto usuarioDto = new UsuarioDto();
+        usuarioDto.setNomeUsuario("teste");
+        return "/home";
     }
 }
